ccba065ce96afdc82028932340d9b672c53759f7
hgc498 <40686263+hgc498@users.noreply.github.com>
2022-03-02 11:43:39 +0000

    Update Airline.java

--- files/Airline.java
+++ files/Airline.java
@@ -1,12 +1,11 @@
 //class to contain airline details - names and codes 
 public class Airline {
 	
-	private String name;
 	private String code;
-	
+	private String name;
 	
 	public Airline(String name, String code) {
-		
+	//if no details have been entered for either the code or the name, throw an illegal exception	
 		if( name.trim().length() ==0|| code.trim().length()== 0)    
         {
           throw new IllegalStateException(
@@ -17,13 +16,18 @@ public class Airline {
 		}
 		
 	}
+	public String getCode() {
+		return code;
+	}
 	
 	public String getAirline() {
 		return name;
 	}
 	
-	public String getCode() {
-		return code;
-	}
+	//String containing all the details of airlines 
+	public String toString()
+    	{
+        return String.format("%-5s", code ) + String.format("%-35s", name);
+    	}
 	
 }
