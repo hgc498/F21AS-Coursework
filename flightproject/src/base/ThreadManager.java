69165918f19dcf92d642c89c300077e3b1704c1f
hgc498 <40686263+hgc498@users.noreply.github.com>
2022-04-13 14:43:31 +0100

    Update ThreadManager.java

--- flightproject/src/base/ThreadManager.java
+++ flightproject/src/base/ThreadManager.java
@@ -15,21 +15,40 @@ public class ThreadManager {
 		
 		// create blocking queue to store positions of flights
 		//
-		BlockingQueue<Flight> positions = new ArrayBlockingQueue<Flight>(3);
+		BlockingQueue<Flight> positions = new ArrayBlockingQueue<Flight>(4);
 		
-		//set up the threads 
-		//tower threads 
+		//set up the control tower threads 
 		ControlTower t1 = new ControlTower(newPosition, rand.nextBoolean(), positions);
 		ControlTower t2 = new ControlTower(newPosition,rand.nextBoolean() , positions);
-		//flight threads 
 		
 		//start the threads 
 		new Thread(t1).start();
 		new Thread(t2).start();
 		
 		
-		//to do 
-		for(int i = 1; i <= 3; i++) {
+		//set up the flight threads 
+		Flight flight1= new Flight(t1, "Flight 1");
+		Flight flight2= new Flight(t1, "Flight 2");
+		Flight flight3= new Flight(t2, "Flight 3");
+		Flight flight4= new Flight(t2, "Flight 4");
+		
+		try{
+			flight1.t.join();;
+			flight2.t.join();
+			flight3.t.join();
+			flight4.t.join();
+			
+			
+		   }
+		//throwing an exception
+		catch(InterruptedException e){
+			System.out.println("Process was Interrupted");
+		}
+		
+		
+		
+		
+		for(int i = 1; i <= 4; i++) {
 			positions.add(new Flight());
 		}
 		
